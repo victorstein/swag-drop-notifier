f198c4bd7fc0f2ce835504ce9d2111d38a6642a5
victorstein <stein.hakase.vs@gmail.com>
2024-09-30 14:21:21 -0600

    ♻️f Removed AnimatedLazy in favor of router loading component

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,13 +1,9 @@
 import '@src/Popup.css';
-import { animatedLazy, useTheme, withErrorBoundary, withSuspense } from '@extension/shared';
+import { withErrorBoundary } from '@extension/shared';
 import { LoadingScreen } from '@extension/ui';
-
-const LazyMain = animatedLazy(() => import('./pages/main'));
 
 const Popup = () => {
   useTheme();
-
-  return <LazyMain />;
 };
 
 export default withErrorBoundary(withSuspense(Popup, <LoadingScreen />), <div> Error Occur </div>);
