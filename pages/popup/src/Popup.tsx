24a9c486e5499c411a3c6e172df3ba05076edcef
victorstein <stein.hakase.vs@gmail.com>
2024-09-30 14:25:13 -0600

    ✨ Added router logic

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,9 +1,11 @@
 import '@src/Popup.css';
 import { withErrorBoundary } from '@extension/shared';
 import { LoadingScreen } from '@extension/ui';
+import { router } from './router';
+import { RouterProvider } from 'react-router-dom';
 
 const Popup = () => {
-  useTheme();
+  return <RouterProvider router={router} fallbackElement={<LoadingScreen />} />;
 };
 
 export default withErrorBoundary(withSuspense(Popup, <LoadingScreen />), <div> Error Occur </div>);
---
+++ pages/popup/src/router/index.tsx
@@ -0,0 +1,37 @@
+import { MainLayout } from '@src/layouts/mainLayout';
+import { createHashRouter, type RouteObject } from 'react-router-dom';
+import Main from '../pages/main';
+import { withLayout } from '@extension/shared';
+
+const routerDefinition: RouteObject[] = [
+  {
+    path: '/',
+    element: <Main />,
+    loader: async (): Promise<null> => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(null);
+        }, 2000);
+      });
+    },
+  },
+];
+
+const addLayoutRecursively = (routerDefinition: RouteObject[]) => {
+  return routerDefinition.map((route: RouteObject) => {
+    if (route.children) {
+      route.children = addLayoutRecursively(route.children);
+    }
+
+    if (!route.element) {
+      return route;
+    }
+
+    return {
+      ...route,
+      element: withLayout(route.element, MainLayout),
+    };
+  });
+};
+
+export const router = createHashRouter(addLayoutRecursively(routerDefinition));
