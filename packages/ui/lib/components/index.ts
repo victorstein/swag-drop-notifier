5796f1499f2b9403057c92e2aa92638cece38d9b
victorstein <stein.hakase.vs@gmail.com>
2024-10-03 09:08:33 -0600

    chore: exporting new components

--- packages/ui/lib/components/index.ts
+++ packages/ui/lib/components/index.ts
@@ -1,3 +1,6 @@
 export * from './Button';
 export * from './LoadingScreen';
 export * from './Text';
+export * from './AuthenticatedRoute';
+export * from './Fade';
+export * from './ThemeSwitcher';
