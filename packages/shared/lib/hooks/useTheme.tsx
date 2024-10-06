a60747c2ab5bb74603b4e4c0540449cf67d20d2a
victorstein <stein.hakase.vs@gmail.com>
2024-10-06 11:27:13 -0600

    refactor: apply theme once store is hydrated

--- packages/shared/lib/hooks/useTheme.tsx
+++ packages/shared/lib/hooks/useTheme.tsx
@@ -2,13 +2,19 @@ import { useEffect } from 'react';
 import { themeStorage } from '@extension/storage';
 
 export const useTheme = () => {
+  const isThemeStorageHydrated = themeStorage.use.isStoreHydrated();
   const theme = themeStorage.use.theme();
+
   useEffect(() => {
+    if (!isThemeStorageHydrated) {
+      return;
+    }
+
     if (theme === 'dark') {
       document.documentElement.classList.add('dark');
     }
     if (theme === 'light') {
       document.documentElement.classList.remove('dark');
     }
-  }, [theme]);
+  }, [theme, isThemeStorageHydrated]);
 };
