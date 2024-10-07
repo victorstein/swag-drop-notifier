aa5b84074e5fb8b1202861abe4d29817106c18ea
victorstein <stein.hakase.vs@gmail.com>
2024-10-06 18:58:21 -0600

    chore: removed unused pages

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -28,37 +28,37 @@ const manifest = deepmerge(
     version: packageJson.version,
     description: '__MSG_extensionDescription__',
     host_permissions: ['<all_urls>'],
-    permissions: ['storage', 'scripting', 'tabs', 'notifications'],
-    options_page: 'options/index.html',
-    background: {
-      service_worker: 'background.iife.js',
-      type: 'module',
-    },
+    permissions: ['storage', 'scripting', 'tabs', 'notifications', 'cookies', 'activeTab'],
+    // options_page: 'options/index.html',
+    // background: {
+    //   service_worker: 'background.iife.js',
+    //   type: 'module',
+    // },
     action: {
       default_popup: 'popup/index.html',
       default_icon: 'icon-34.png',
     },
-    chrome_url_overrides: {
-      newtab: 'new-tab/index.html',
-    },
+    // chrome_url_overrides: {
+    //   newtab: 'new-tab/index.html',
+    // },
     icons: {
       128: 'icon-128.png',
     },
-    content_scripts: [
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content-ui/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        css: ['content.css'], // public folder
-      },
-    ],
-    devtools_page: 'devtools/index.html',
+    // content_scripts: [
+    //   {
+    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //     js: ['content/index.iife.js'],
+    //   },
+    //   {
+    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //     js: ['content-ui/index.iife.js'],
+    //   },
+    //   {
+    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //     css: ['content.css'], // public folder
+    //   },
+    // ],
+    // devtools_page: 'devtools/index.html',
     web_accessible_resources: [
       {
         resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
@@ -66,7 +66,7 @@ const manifest = deepmerge(
       },
     ],
   },
-  !isFirefox && sidePanelConfig,
+  // !isFirefox && sidePanelConfig,
 );
 
 export default manifest;
