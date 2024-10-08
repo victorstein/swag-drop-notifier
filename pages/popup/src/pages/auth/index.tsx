8981abfea854e384550144f4d8aea52b2b270b61
victorstein <stein.hakase.vs@gmail.com>
2024-10-08 09:59:21 -0600

    feat: implemented new loading animation

--- pages/popup/src/pages/auth/index.tsx
+++ pages/popup/src/pages/auth/index.tsx
@@ -1,11 +1,16 @@
+import { authStorage } from '@extension/storage';
 import { Button, Fade, Text } from '@extension/ui';
-import { useCallback, useEffect, type FC } from 'react';
+import { useCallback, useEffect, useState, type FC } from 'react';
+import { useInterval } from 'usehooks-ts';
 
 const Title = Text.Title;
 const Paragraph = Text.Paragraph;
 export interface LoginProps {}
 
 export const Login: FC<LoginProps> = () => {
+  const [interval, setInterval] = useState<number | null>(null);
+  const setAuthCookie = authStorage.use.setAuthCookie();
+
   const redirectHandler = useCallback(() => {
     chrome.tabs.create({ url: 'https://shop.lumenalta.com' });
   }, []);
@@ -13,10 +18,51 @@ export const Login: FC<LoginProps> = () => {
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
       if (tabs[0].url?.includes('shop.lumenalta.com')) {
-        console.log('We are at the shop');
+        setInterval(1000);
+      } else {
+        setInterval(null);
       }
     });
   }, []);
+
+  useInterval(() => {
+    chrome.cookies.get(
+      {
+        url: 'https://shop.lumenalta.com',
+        name: 'storefront_digest',
+      },
+      cookie => {
+        if (cookie) {
+          setAuthCookie(cookie.value);
+          chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            if (tabs[0].url?.includes('shop.lumenalta.com')) {
+              chrome.tabs.remove(tabs[0].id!);
+            }
+          });
+        }
+      },
+    );
+  }, interval);
+
+  if (interval !== null) {
+    return (
+      <div className="flex flex-1 items-start justify-center flex-col">
+        <Fade fromTop delay={0.2}>
+          <Title>Almost there!</Title>
+        </Fade>
+        <Fade fromTop delay={0.4} className="mb-6">
+          <Paragraph>We are waiting for you to login to the Lumenalta store</Paragraph>
+        </Fade>
+        <Fade fromTop delay={0.6} className="flex w-full justify-center">
+          <img
+            className="w-16 h-16 animate-[spin_1.7s_cubic-bezier(0.22,0.61,0.36,1)_infinite] dark:filter dark:grayscale dark:brightness-0 dark:invert"
+            src={chrome.runtime.getURL('popup/logo-alone.svg')}
+            alt="spinner"
+          />
+        </Fade>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-1 items-start justify-center flex-col">
