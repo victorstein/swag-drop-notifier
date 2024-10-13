f50c22dfbe102ab5abfd0eefca2e1c13ffcb0c79
victorstein <stein.hakase.vs@gmail.com>
2024-10-13 03:23:20 -0600

    feat: created main page

--- pages/popup/src/pages/main/index.tsx
+++ pages/popup/src/pages/main/index.tsx
@@ -1,20 +1,44 @@
+import { useFetchProducts } from '@extension/shared';
 import { authStorage } from '@extension/storage';
-import { Button, Fade, Text } from '@extension/ui';
+import { Fade, LumenSpinner, Product, Text } from '@extension/ui';
 import { MainLayout } from '@src/layouts/mainLayout';
 
-const Title = Text.Title;
+const { Title, Paragraph } = Text;
 
 const Main = () => {
-  const resetAuthStore = authStorage.use.resetAuthStore();
+  const { data, error, isLoading } = useFetchProducts();
+  const resetAuthStorage = authStorage.use.resetAuthStore();
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <LumenSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    setTimeout(() => {
+      resetAuthStorage();
+    }, 5000);
+
+    return (
+      <div className="flex flex-1 items-center justify-center flex-col">
+        <Title className="text-center">Error:</Title>
+        <Paragraph className="text-center">
+          {error?.message ?? 'There was an error while fetching the products. Please try again later.'}
+        </Paragraph>
+      </div>
+    );
+  }
 
   return (
-    <div className="flex flex-col items-start justify-start">
-      <Fade delay={0.2}>
-        <Title>Main Content</Title>
-      </Fade>
-      <Fade delay={0.4}>
-        <Button onClick={resetAuthStore}>Logout</Button>
-      </Fade>
+    <div className="flex flex-wrap mb-4">
+      {data.map((product, index) => (
+        <Fade key={product.id} className="w-[130px] flex h-52 mb-2 odd:mr-2" delay={index < 6 ? 0.2 * index : 0.2}>
+          <Product product={product} />
+        </Fade>
+      ))}
     </div>
   );
 };
