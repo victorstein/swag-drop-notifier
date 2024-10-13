9bfee3cd9059e1af6f72dec5934cf771b01940ac
victorstein <stein.hakase.vs@gmail.com>
2024-10-13 03:23:50 -0600

    feat: setup for data fetching and notification handling

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,11 +1,25 @@
 import '@src/Popup.css';
-import { withErrorBoundary } from '@extension/shared';
+import { useChromeBadge, withErrorBoundary } from '@extension/shared';
 import { LoadingScreen } from '@extension/ui';
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
+const queryClient = new QueryClient();
 
 const Popup = () => {
-  return <RouterProvider router={router} fallbackElement={<LoadingScreen />} />;
+  const { clearBadge } = useChromeBadge();
+
+  useEffect(() => {
+    clearBadge();
+  }, [clearBadge]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} fallbackElement={<LoadingScreen />} />
+    </QueryClientProvider>
+  );
 };
 
 export default withErrorBoundary(Popup, <div> Error Occur </div>);
